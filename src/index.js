28b1ffa7427e3dddfcfb1ba0ea645e60405b7945
Maxim Dejnew <red41tiger@mail.ru>
2021-11-29 14:58:47 +0700

    Some manipulations

--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
 import Main from './index.js';
-import indexCss from './index.css' assert { type: 'css' };
-import commonCss from '/assets/styles/common.css' assert { type: 'css' };
-
-const main = new Main();
-
-main.render().then( () => console.log('Страница готова!') );
+import indexCss from './index.css';
