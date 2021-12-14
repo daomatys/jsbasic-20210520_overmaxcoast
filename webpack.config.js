574bdf371e0d9d33502d0dd50d0f987534fbd01f
Maxim Dejnew <red41tiger@mail.ru>
2021-12-14 19:59:28 +0700

    Remover copy-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const PATHS = {
   src: path.join(__dirname, '/src'),
   dist: path.join(__dirname, '/dist')
 }
+
+const PAGES__ROOT = path.join( PATHS.src, 'pages' );
 
 module.exports = {
   mode: 'production',
